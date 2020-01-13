3464296245199916260fddd275d2e47d9ddd96d8
fangh1 <18613102586@126.com>
2020-01-14 00:30:19 +0800

    test

--- nocosdemo/customer/src/main/java/com/nacos/customer/FeignClent.java
+++ nocosdemo/customer/src/main/java/com/nacos/customer/FeignClent.java
@@ -6,5 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(value = "provider")
 public interface FeignClent {
     @GetMapping("/service")
+
     public String getConfig();
 }
